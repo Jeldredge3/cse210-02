74bc77ddc45a43e2083463befd2784a96a05f641
Jeldredge3 <96218230+Jeldredge3@users.noreply.github.com>
2022-05-05 08:22:43 -0600

    Updated 'director.py'
    Added a comment to explain that this file is used to start the program.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,6 @@
+""" CSE210 W03 - High Low """
+# The game will be started using this file.
+
 from game.director import Director
 
 director = Director() # Object is calling a class called Director.
