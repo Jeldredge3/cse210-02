5104c309c2109550557310f96478a7d75a438244
Jeldredge3 <96218230+Jeldredge3@users.noreply.github.com>
2022-05-05 08:09:43 -0600

    Added the 'director' and the '__main__' files
    Uploaded 'director.py' which sets up the game and '__main__.py' which will be used to start the game.

---
+++ __main__.py
@@ -0,0 +1,4 @@
+from game.director import Director
+
+director = Director() # Object is calling a class called Director.
+director.start_game() # Object is calling a variable/attribute within in the Director class to start the game.
