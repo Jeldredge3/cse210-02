ee0ead1cbe33bc8c75f18dde8bd45cec9b17184b
jeldredge3 <jordanmeldredge@gmail.com>
2022-05-09 16:25:07 -0600

    Made the To Do List smaller

--- game/director.py
+++ game/director.py
@@ -3,89 +3,20 @@
 from game.playing_cards import DeckofCards
 
 # TODO: 
-# 1) Draw the first card for the user to see, which will then ask if the following card is Higher/Lower.
-# 2) Make sure get_inputs(self) method works.
-# 3) Update the game's score if the user guesses Higher/Lower than the drawn card value.
-# 4) Display the score to the user after each round, ask if they want to keep playing, end the game if they decide to quit.
+# 1) Compare card value with user input
+# 2) Make score update if player guesses correctly or incorrectly.
+# 3) Any other fun additions to the game.
 
 class Director:
-    """ A person who directs the game. 
-    
-    The responsibility of a Director is to control the sequence of play.
+    """ The person who directs the game. Controls the sequence of play.
 
-    Attributes: (To be created)
-        deck_cards (List[Cards]): A list of card instances.
+    Attributes:
         is_playing (boolean): Whether or not the game is being played.
         score (int): The score for one round of play.
         total_score (int): The accumulated score for the entire game.
     """
 
     def __init__(self):
-        """ Constructs a new Director.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
-        print("--------------------")
-
-        self.card = []
-        self.is_playing = True
-        self.score = 0
-        self.total_score = 0
-
-        #self.start_game()
-        self.get_card_from_deck()
-
-    def start_game(self):
-        """ Starts the game by running the main game loop.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
-        while self.is_playing:
-            self.get_inputs() # Get user input.
-            self.do_updates() # Update the game based after checking user input.
-            self.do_outputs() # Display user output.
-        
-
-    def get_inputs(self):
-        """ Ask the user if they guess that the next card will be higher or lower in value.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        higher_lower = input("Higher or Lower? [h/l] ")
-        self.is_playing = (higher_lower == "h" or higher_lower == "l") # If user types 'h' or 'l', they are considered actively playing.
-
-    def do_updates(self):
-        """ Updates the player's score.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        if not self.is_playing:
-            return 
-
-        pass 
-        # To be updated later, will need to update the player score if they are still playing.
-
-    def do_outputs(self):
-        """ Displays the card that was drawn as well as the score. Also asks the player if they want to keep playing. 
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        if not self.is_playing:
-            return
-        
-        drawn_card = ""
-        # To be update later, will need to somehow draw a card from a deck.
-
-        print(f"Next card was: {drawn_card}")
-        print(f"Your score is: {self.total_score}\n")
-        self.is_playing == (self.score > 0)
-
-    def get_card_from_deck(self):
         """ Create a deck of 52 cards using the 'DeckofCards' class in 'playing_cards.py'.
         The deck is generated using the '.create_deck' method and is stored within a list.
         An individual card can be accessed from the list by calling its index. (ex. 'deck[0]')
@@ -108,33 +39,93 @@ class Director:
             .create_deck()      Builds the Deck of cards using a list of objects.
             .shuffle_deck()     Re-orders the list of objects in the Deck.
             .count_deck()       Returns the amount of cards in the Deck.
-            .draw_card()        Draws a card from the Deck.
-            .discard_hand()     Discards all cards in your hand.
-            .merge_deck()       Combines all discarded cards back into the deck.
+            .draw_card()        Draws a card from the Deck.                         (Parameters: Deck, Hand)
+            .discard_hand()     Discards all cards in Hand.                         (Parameters: Hand, Discard)
+            .merge_discarded()  Combines all discarded cards back into the Deck.    (Parameters: Discard, Deck)
             .view_top_card()    Prints the card name in position [0] of a list.
             .view_list()        Prints each card name within a list.
-            .view_all_lists()   Prints each list. (Parameters: Deck, Hand, Discard)
+            .view_all_lists()   Prints each list.                                   (Parameters: Deck, Hand, Discard)
         """
+        # Establish whether the game is being played. Set variables for the score.
+        self.is_playing = True
+        self.score = 0
+        self.total_score = 0
 
-        # Create an empty list to pass through the 'DeckofCards' class. Store deck within an object, access card objects from the list.
-        deck = []
-        deck_obj = DeckofCards()
-        deck_obj.create_deck(deck)
-        # Create an empty list to store any drawn cards taken from the deck list.
-        hand = []
-        # Create an empty list to store any cards discarded from the hand list.
-        discard = []
-        # Access any attributes from a card or methods/functions from the deck below this line.
-        deck_obj.view_top_card(deck)
-        deck_obj.view_list(hand)
+        # Create the deck and set up the game.
+        self.deck = [] # Holds a list of individual card objects.
+        self.hand = [] # Temporarily holds any cards that are drawn from the deck list.
+        self.discard = [] # Will contain any cards that are discarded from the hand list.
 
-        deck_obj.draw_card(deck, hand)
+        # Store deck data in an object called 'hilo' created from the class 'DeckofCards'.
+        self.hilo = DeckofCards()
+        self.hilo.create_deck(self.deck)
+
+        # Start the game.
+        self.game_setup()
+        self.start_game()
+
+    def game_setup(self):
+        # Print the game name and initial message.
+        print("-----------------------")
+        print("       HiLo Game       ")
+        print("- - - - - - - - - - - -")
+        print("Instructions:")
+        print("Try to guess if the next card will be of higher or lower value than the previous.")
+        print("-----------------------\n")
+
+    def start_game(self):
+        """ Starts the game by running the main game loop.
         
-        deck_obj.view_top_card(deck)
-        deck_obj.view_list(hand)
-        deck_obj.view_list(discard)
-        print("--------------------------")
-        deck_obj.view_all_lists(deck, hand, discard)
-        print("--------------------------")
-        deck_obj.view_all_lists(deck, hand, discard)
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.get_inputs() # Get user input.
+            self.do_updates() # Update the game after checking user input.
+            self.do_outputs() # Display user output.
 
+    def get_inputs(self):
+        """ Draw a card from the Deck.
+            Ask the user if they guess that the next card will be higher or lower in value.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        # Draw a card. Store the card's attributes in variables.
+        self.hilo.draw_card(self.deck, self.hand)
+        card_name = self.hand[0].name
+        card_value = self.hand[0].value
+        # Ask user if the next card will be in higher value.
+        higher_lower = input("Higher or Lower? [h/l] ")
+        self.is_playing = (higher_lower == "h" or higher_lower == "l") # If user types 'h' or 'l', they are considered actively playing.
+
+    def do_updates(self):
+        """ Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return 
+
+        # TODO: Compare the user input with the card's value.
+
+        # Discard the drawn card so it is not placed back in the Deck.
+        self.hilo.discard_card(self.hand, self.discard)
+        # TEST: self.hilo.view_all_lists(self.deck, self.hand, self.discard)
+
+    def do_outputs(self):
+        """ Displays Score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+        
+        drawn_card = ""
+        # TODO: Update the user score.
+
+        print(f"Next card was: {drawn_card}")
+        print(f"Your score is: {self.total_score}\n")
+        self.is_playing == (self.score > 0)
